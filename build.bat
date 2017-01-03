dbc9876d5a3f5c09ae6651dea61c8581695e7216
Eros Stein <eros.stein@gmail.com>
2017-01-03 10:42:28 -0200

    Final build.bat test.

--- build.bat
+++ build.bat
@@ -14,10 +14,10 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-"%programfiles(x86)%\MSBuild\14.0\Bin\msbuild" Blayer.Data\Blayer.Data.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%MsBuildExe% Blayer.Data\Blayer.Data.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net46
 
-%nuget% pack "Blayer.Data\Blayer.Data.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%NuGet% pack "Blayer.Data\Blayer.Data.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
